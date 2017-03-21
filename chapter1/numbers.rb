8b8d95f648506b8ed95472c09f89fc70ced7de47
Skyler Olds <skylerolds@gmail.com>
2017-03-21 08:35:52 -0600

    initial commit

---
+++ chapter1/numbers.rb
@@ -0,0 +1,5 @@
+puts "There are #{365 * 24} hours in a year."
+puts "There are #{3650 * 24 * 60} minutes in a decade."
+puts "I am #{365 * 21 * 24 * 60 * 60} seconds old."
+puts "I hope to eat #{(1000..10000).to_a.sample} chocolates in my lifetime."
+puts "If you are 1246 million seconds old, you are #{1246000000 / 60 / 60 / 24 / 365} years old."
